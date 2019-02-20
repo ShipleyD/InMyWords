2f0cabaa5b458145a8fe68a8cff4b497c1f5d3ba
Shipley D <46893629+ShipleyD@users.noreply.github.com>
2019-02-20 17:41:51 +0000

    set up of firebase

--- app/src/main/java/com/example/inmywords/HomeActivity.java
+++ app/src/main/java/com/example/inmywords/HomeActivity.java
@@ -1,22 +1,35 @@
 package com.example.inmywords;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity  implements
         View.OnClickListener{
 
+    private static final String TAG = "HomeActivity";
     private Button btnSearch;
     private Button btnAdd;
     private Button btnFavourites;
+    private FirebaseFirestore db;
 
 
     @Override
@@ -40,8 +53,16 @@ public class HomeActivity extends AppCompatActivity  implements
         btnAdd.setOnClickListener(this);
         btnFavourites.setOnClickListener(this);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Enable Firestore logging
+        FirebaseFirestore.setLoggingEnabled(true);
 
+        initFirestore();
+    }
+
+    private void initFirestore() {
+
+        // Access a Cloud Firestore instance from your Activity
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
